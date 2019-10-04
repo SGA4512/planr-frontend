f1b737ba0227583b5697b9944c485a525940443e
lilyhoratio <lilyhzhou@gmail.com>
2019-10-03 19:31:03 -0700

    changed baseurl to deployed backend

--- src/services/axiosWithAuth.js
+++ src/services/axiosWithAuth.js
@@ -4,8 +4,8 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    // baseURL: "https://planr-backend.herokuapp.com/api",
-    baseURL: "http://localhost:8000",
+    baseURL: "https://planr-backend.herokuapp.com/api",
+    // baseURL: "http://localhost:8000",
     headers: {
       Authorization: token
     }
